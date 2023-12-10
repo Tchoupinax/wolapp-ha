c09c7aebd6feeab8599caea717a8a686e60a8140
Tchoupinax <corentinfiloche@hotmail.fr>
2023-12-10 12:30:22 +0100

    feat: successful installation in HA

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 ssh ha "mkdir -p /tmp/wolapp"
-scp -O Dockerfile config.yaml build.yaml ha:/tmp/wolapp
+scp -O config.yaml ha:/tmp/wolapp
+scp -O build.yaml ha:/tmp/wolapp
 
 ssh ha "sudo mkdir -p /addons/wolapp"
 ssh ha "sudo cp /tmp/wolapp/* /addons/wolapp"
